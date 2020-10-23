d7d188810b9f6a67271836b68a4ab0d861c2e366
Jay Shi <JayShi@eurofins.com>
2020-10-23 17:24:09 +0800

    Adding Unit Test

---
+++ Presentation.Wpf.PrimeNumber.UnitTests/Actions/PrimeNumberActionTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Presentation.Wpf.PrimeNumber.Actions;
+
+namespace Presentation.Wpf.PrimeNumber.UnitTests.Actions
+{
+    [TestClass]
+    public class PrimeNumberActionTest
+    {
+        private PrimeNumberAction _action;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _action = new PrimeNumberAction();
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void PrimeNumberAction_GIVEN_A_prime_number_WHEN_IsPrimeNumber_THEN_true_return()
+        {
+            // Arrange
+            var primeNumber = 17;
+
+            // Act
+            var result = _action.IsPrimeNumber(primeNumber);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void PrimeNumberAction_GIVEN_A_prime_number_WHEN_IsPrimeNumberImproved_THEN_true_return()
+        {
+            // Arrange
+            var primeNumber = 17;
+
+            // Act
+            var result = _action.IsPrimeNumberImproved(primeNumber);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+
+
+}
